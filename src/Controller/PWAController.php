00cf962e0ad6f2541e4255008a0c44f4b341a656
Théodore BIADALA <theodore@biadala.net>
2017-10-07 18:07:50 +0200

    Use importScripts to remove db cache

--- src/Controller/PWAController.php
+++ src/Controller/PWAController.php
@@ -11,17 +11,12 @@ use Symfony\Component\HttpFoundation\Response;
 /**
  * Default controller for the pwa module.
  */
-class DefaultController extends ControllerBase {
+class PWAController extends ControllerBase {
 
-  public function pwa_serviceworker_file_data($version = 1) {
-    $data =  \Drupal::cache()->get('pwa:serviceworker');
-    if ($data) {
-      $data = $data->data;
-    }
-    else {
-      $data = pwa_serviceworker_file();
-      \Drupal::cache()->set('pwa:serviceworker', $data);
-    }
+  public function pwa_serviceworker_file_data() {
+    $query_string = \Drupal::state()->get('system.css_js_query_string') ?: 0;
+    $path = drupal_get_path('module', 'pwa');
+    $data =  'importScripts("/' . $path . '/js/serviceworker.js?' . $query_string . '");';
 
     return new Response($data, 200, [
       'Content-Type' => 'application/javascript',
