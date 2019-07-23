b31abde5dc73a409a700830b9eadfe5fdd7d502b
AlexBorsody <AlexBorsody@473596.no-reply.drupal.org>
2019-07-23 18:06:35 -0400

    Issue #3060759 by AlexBorsody, scuba_fly, MrPaulDriver, lfjimenez, chrisdotbm, rupl, prics, nimoatwoodway, mikael_ek, gulab.bisht, martin107, geoffreyr, fmr, nginex, ArnoVDC, NigelCunningham: Fully working D8 version based on D7 Serviceworker

--- js/serviceworker-load.js
+++ js/serviceworker-load.js
@@ -1,47 +1,16 @@
-(function(drupalSettings) {
-  if (!("serviceWorker" in navigator)) {
-    return;
+(function ($, Drupal, drupalSettings, navigator, window) {
+  'use strict';
+
+  if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+      navigator.serviceWorker .register("/serviceworker-pwa", { scope: drupalSettings.path.baseUrl })
+        .then(registration => {
+          console.log(`Service Worker registered! Scope: ${registration.scope}`);
+        })
+        .catch(err => {
+          console.log(`Service Worker registration failed: ${err}`);
+        });
+    });
   }
 
-  const documentElement = document.documentElement;
-  const width = documentElement.clientWidth;
-  const height = documentElement.clientHeight;
-
-  function loadPage(url) {
-    let iframe = document.createElement("iframe");
-    // When loaded remove from page.
-    iframe.addEventListener("load", event => {
-      iframe.remove();
-      iframe = null;
-    });
-    iframe.setAttribute("width", width);
-    iframe.setAttribute("height", height);
-    iframe.setAttribute("style", "position:absolute;top:-110%;left:-110%;");
-    iframe.setAttribute("src", url);
-    document.body.appendChild(iframe);
-  }
-
-  navigator.serviceWorker
-    .register("/serviceworker-pwa", { scope: "/" })
-    .then(registration => {
-      // Only add default pages to cache if the SW is being installed.
-      if (registration.installing) {
-        // open the pages to cache in an iframe because assets are not
-        // predictable.
-        drupalSettings.pwa.precache.forEach(loadPage);
-      }
-    });
-
-  // Reload page when user is back online on a fallback offline page.
-  window.addEventListener("online", function() {
-    const loc = window.location;
-    // If the page served is the offline fallback, try a refresh when user
-    // get back online.
-    if (
-      loc.pathname !== "/offline" &&
-      document.querySelector("[data-drupal-pwa-offline]")
-    ) {
-      loc.reload();
-    }
-  });
-})(drupalSettings);
+})(jQuery, Drupal, drupalSettings, navigator, window);
