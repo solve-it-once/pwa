026fc722175d9983028e870054e45e232a4f61a6
nimoatwoodway <nimoatwoodway@2683961.no-reply.drupal.org>
2020-02-03 12:27:30 -0500

    Issue #3038231 by nimoatwoodway, rupl, AlexBorsody, ChristophWeber, grathbone: JS compatibility for older browsers

--- js/serviceworker-load.js
+++ js/serviceworker-load.js
@@ -1,16 +1,17 @@
+"use strict";
+
 (function (Drupal, drupalSettings, navigator, window) {
   'use strict';
 
   if ('serviceWorker' in navigator) {
-    window.addEventListener('load', () => {
-      navigator.serviceWorker .register("/serviceworker-pwa", { scope: drupalSettings.path.baseUrl })
-        .then(registration => {
-          console.log(`Service Worker registered! Scope: ${registration.scope}`);
-        })
-        .catch(err => {
-          console.log(`Service Worker registration failed: ${err}`);
-        });
+    window.addEventListener('load', function () {
+      navigator.serviceWorker.register("/serviceworker-pwa", {
+        scope: drupalSettings.path.baseUrl
+      }).then(function (registration) {
+        console.log("Service Worker registered! Scope: ".concat(registration.scope));
+      }).catch(function (err) {
+        console.log("Service Worker registration failed: ".concat(err));
+      });
     });
   }
-
 })(Drupal, drupalSettings, navigator, window);
